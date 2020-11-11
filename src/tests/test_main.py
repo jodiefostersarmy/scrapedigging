53d12bd2b156f4bf8a875f30cc31d5dbe5622673
Germs <anthony.kwy@gmail.com>
2020-11-11 13:10:45 +1100

    test files updated and dummy test working fine

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,2 +1,6 @@
-def test_add(self):   
-    self.assertEqual(add(2,3), 5)
+import unittest
+from main import add
+
+class TestDummyFunctions(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2,3), 5)
